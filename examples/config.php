c608958bf8d108762ced83943b85c7310dae8e4c
imhui <seasonlyh@gmail.com>
2016-03-25 16:33:40 +0800

    update

--- examples/config.php
+++ examples/config.php
@@ -6,6 +6,6 @@
  * Time: 15:27
  */
 
-define('AppId', 'd4551e91fc7200c817');
-define('AppSecret', '8677df4220f08f5586a16a8da1956564');
+define('AppId', '');
+define('AppSecret', '');
 define('CacheDIR', 'cache');
